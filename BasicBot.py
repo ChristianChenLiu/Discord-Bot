b756d3276a16f3f0e47b4de11e1cb6cf18da1aff
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-08-21 12:24:46 -0500

    Bot console log in notification

--- BasicBot.py
+++ BasicBot.py
@@ -3,5 +3,8 @@ from dotenv import load_dotenv
 
 client = discord.Client(intents=discord.Intents.default())
 
-load_dotenv()
-client.run(os.getenv("BASIC_DISCORD_BOT"))
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+client.run(os.getenv("BASIC_DISCORD_TOKEN"))
