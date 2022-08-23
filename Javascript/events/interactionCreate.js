fdb608fbe890a0f38cc2b260c16cf5778f568e1c
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-08-23 14:45:51 -0500

    intereactionCreate event

---
+++ Javascript/events/interactionCreate.js
@@ -0,0 +1,10 @@
+/** @format */
+
+module.exports = {
+	name: 'interactionCreate',
+	execute(interaction) {
+		console.log(
+			`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
+		);
+	}
+};
